6dc8931f8f65de11d8f8a9d0933e081b6e87d129
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2024-04-23 10:20:44 +0200

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,11 +7,20 @@ def rotate_2d_matrix(matrix):
    """
     Rotate a 2D matrix 90 degrees clockwise.
     """
-    for i in range(int(len(matrix) / 2)):
-        for j in range(i, (len(matrix) - i - 1)):
-            x = (len(matrix) - 1 - j)
-            tmp = matrix[i][j]
-            matrix[i][j] = matrix[x][i]
-            matrix[x][i] = matrix[(len(matrix) - i - 1)][x]
-            matrix[(len(matrix) - i - 1)][x] = matrix[j][(len(matrix) - i - 1)]
-            matrix[j][(len(matrix) - i - 1)] = tmp  
+     n = len(matrix)
+    # Iterate through each layer of the matrix
+    for layer in range(n // 2):
+        first = layer
+        last = n - 1 - layer
+        # Iterate through each element in the current layer
+        for i in range(first, last):
+            # Store the current element in a temporary variable
+            temp = matrix[layer][i]
+            # Move values from right to top
+            matrix[layer][i] = matrix[-i - 1][layer]
+            # Move values from bottom to right
+            matrix[-i - 1][layer] = matrix[-layer - 1][-i - 1]
+            # Move values from left to bottom
+            matrix[-layer - 1][-i - 1] = matrix[i][-layer - 1]
+            # Move values from top to left
+            matrix[i][-layer - 1] = temp
